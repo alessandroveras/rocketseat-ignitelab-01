a263ad876bf3c13dbefec788add355c35bef2585
alessandroveras <alessandro.veras@gmail.com>
2022-04-06 02:46:44 -0300

    graphql implementation

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TestController } from './test.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import { DatabaseModule } from 'src/database/database.module';
+import * as path from 'path';
+import { TestResolver } from './test.resolver';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [TestController],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
+    }),
+  ],
+  providers: [TestResolver],
 })
 export class HttpModule {}
