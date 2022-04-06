c95917098a4f062421a699661ca1a15ac93bce0f
alessandroveras <alessandro.veras@gmail.com>
2022-04-06 01:38:13 -0300

    frontend authentication

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,29 +1,29 @@
-import { getAccessToken, useUser } from "@auth0/nextjs-auth0";
+import { getAccessToken, getSession, useUser } from "@auth0/nextjs-auth0";
 import { GetServerSideProps } from "next";
 import Link from "next/link";
 
 export default function Home() {
-  const { user } = useUser();
-
-  return (
-    <div>
-      <h1>Hello World. I'm Veras!</h1>
-      <h2>This is the FrontEnd application</h2>
-
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-
-      <Link href="/api/auth/login">
-        <a>Login</a>
-      </Link>
-    </div>
-  );
+  return null;
 }
 
 export const getServerSideProps: GetServerSideProps = async ({req, res}) => {
-  const token = getAccessToken(req, res);
+  const session = getSession(req, res);
   
-  console.log(token);
-  return {
-    props: {},
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/api/auth/login',
+        permanent: false,
+      }
+    }
+  } else {
+    return {
+      redirect: {
+        destination: '/app',
+        permanent: false,
+      }
+    }
+
   }
+  
 }
